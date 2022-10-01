a3dcf744f9dc76f835957bd4316795be5630892f
Viktor <korolev-viktor92@mail.ru>
2022-10-01 10:30:57 +0400

    Задача 27

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine("Введите число : ");
+/*Console.WriteLine("Введите число : ");
 int A = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите степень : ");
@@ -21,22 +21,27 @@ int fact = B;
     result = result * number;
   }
 return result;
-}
-
-// int Factorial()
-// {
-//   int result = 2;
-//     for (int i = 1; i <= B ; i++)
-//     {
-//         result = result * result;
-//     }
-//     return result;
-// }
+}*/
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {a} равна {GetSum()}");
+
+int GetSum()
+{
+int sum = 0;
+int b = a;
+while (b > 0)
+  {
+  sum = sum + b % 10;
+  b = b / 10 ;
+  }
+return sum;
+}
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
